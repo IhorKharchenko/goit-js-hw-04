812758c94fd31e26e3a02b2d3e4604c11bc426f0
Ihor Kharchenko <ihor.stavr@gmail.com>
2020-10-15 12:00:11 +0300

    homework 4

--- js/task-5.js
+++ js/task-5.js
@@ -1,32 +1,42 @@
-function getAllPropValues(array, prop) {
-  "use strict";
-  // Write code under this line
-  let message = [];
-  for (const object of array) {
-    if (prop in object) {
-      message.push(object[prop]);
-    }
-  }
+const inventory = {
+  items: ["Knife", "Gas mask"],
+  add(itemName) {
+    this.items.push(itemName);
+    return `Adding ${itemName} to inventory`;
+  },
+  remove(itemName) {
+    this.items = this.items.filter((item) => item !== itemName);
+    return `Removing ${itemName} from inventory`;
+  },
+};
 
-  return message;
-}
+const invokeInventoryAction = function (itemName, action) {
+  const act = action(itemName);
+  const msg = `Invoking action on ${itemName}`;
+  return { act, msg };
+};
 
-// Объекты и ожидаемый результат
-const products = [
-  { name: "Радар", price: 1300, quantity: 4 },
-  { name: "Радар", price: 1280, quantity: 2 },
-  { name: "Радар", price: 1320, quantity: 1 },
-  { name: "Сканер", price: 2700, quantity: 1 },
-  { name: "Сканер", price: 2500, quantity: 3 },
-  { name: "Дроид", price: 400, quantity: 7 },
-  { name: "Захват", price: 1200, quantity: 2 },
-];
+const invokeAdd = invokeInventoryAction(
+  "Medkit",
+  inventory.add // Write code in this line
+);
+const arrayAdd = [...inventory.items];
 
-console.log(getAllPropValues(products, "name"));
-// ['Радар', 'Радар', 'Радар', 'Сканер', 'Сканер', 'Дроид', 'Захват']
+console.log(invokeAdd);
+//{ act: 'Adding Medkit to inventory', msg: 'Invoking action on Medkit' }
 
-console.log(getAllPropValues(products, "quantity"));
-// [4, 2, 1, 1, 3, 7, 2]
+console.log(arrayAdd);
+// ['Knife', 'Gas mask', 'Medkit']
 
-console.log(getAllPropValues(products, "category"));
-//  []
+const invokeRemove = invokeInventoryAction(
+  "Gas mask",
+  inventory.remove // Write code in this line
+);
+
+const arrayRemove = [...inventory.items];
+
+console.log(invokeRemove);
+//{ act: 'Removing Gas mask from inventory', msg: 'Invoking action on Gas mask' }
+
+console.log(arrayRemove);
+// ['Knife', 'Medkit']
